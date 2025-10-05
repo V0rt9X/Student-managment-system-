24620c18db4c42a26b647e6d6db9b8f74fb66d5d
V0rt9X <vortexskyshaker@VorteXs-MacBook-Air.local>
2025-10-05 18:20:00 +0200

    feat: Enhance student module with security and contact data
    This commit adds two major features to the student creation process: password encryption and contact information storage.
    
    - A ceasarEnc function was implemented to encrypt user passwords with a Caesar cipher, adding a basic layer of security.
    - The student struct was extended with a union (contactInfo) and an enum (contactType) to allow storing either an email address or a phone number.
    - The createStudent function signature and implementation were updated to accept, encrypt, and store this new information.

--- Deanery/modules/student.c
+++ Deanery/modules/student.c
@@ -1,19 +1,54 @@
 #include "student.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-student* createStudent(int id, const char* name, const char* surname, grade gradeOfStudent, const char* password) {
+// Function of Ceasar cipher.
+static void ceasarEnc(char buffer[],size_t size,const char* str) {
+    const unsigned key=5;
+    snprintf(buffer, size, "%s", str);
+    size_t buffLen = strlen(buffer);
+
+    for (int i = 0; i < buffLen; i++) {
+        if (buffer[i] > 31 && buffer[i] < 127) {
+            buffer[i] = (char)(((buffer[i] - 32 + key) % 95) + 32);
+        }
+    }
+}
+
+student* createStudent(int id, const char* name, const char* surname, grade gradeOfStudent, const char* password,
+                       contactType conType,const char* correspondence) {
+
+    // Allocating memory for structure "student"
     student *s = (student*)malloc(sizeof(student));
     if (!s) {
         printf("Error allocating memory for student\n");
         return NULL;
     }
 
+    // Ceasar cipher for encryption user password
+    size_t bufferSize = sizeof(s->password);
+    char buffer[bufferSize];
+    ceasarEnc(buffer,bufferSize,password);
+
+
+    // Assigning data to a new structure
     s->id = id;
-    snprintf(s->name, 50, "%s", name);
-    snprintf(s->surname, 50, "%s", surname);
+    snprintf(s->name, sizeof(s->name), "%s", name);
+    snprintf(s->surname, sizeof(s->surname), "%s", surname);
     s->gradeOfStudent = gradeOfStudent;
-    snprintf(s->password, 20, "%s", password);
+    snprintf(s->password, sizeof(s->password), "%s", buffer);
+    if (conType == EMAIL) {
+        s->contactOfStudent = EMAIL;
+        snprintf(s->correspondence.eMail,sizeof(s->correspondence.eMail),"%s",correspondence);
+    }else if (conType == PHONE) {
+        s->contactOfStudent = PHONE;
+        snprintf(s->correspondence.phone,sizeof(s->correspondence.phone),"%s",correspondence);
+    }else {
+        s->contactOfStudent = NONE;
+        snprintf(s->correspondence.eMail, sizeof(s->correspondence.eMail), "Address of correspondence wasn't added.");
+    }
 
     return s;
 }
+
--- Deanery/modules/student.h
+++ Deanery/modules/student.h
@@ -3,15 +3,27 @@
 
 typedef enum { F=1,D,C,B,A } grade;
 
+typedef enum { NONE=0, PHONE ,EMAIL } contactType;
+
+typedef union {
+    char eMail[50];
+    char phone[20];
+} contactInfo;
+
 typedef struct {
     int id;
     char name[50];
     char surname[50];
     grade gradeOfStudent;
     char password[20];
+    contactType contactOfStudent;
+    contactInfo correspondence;
 } student;
 
-//The function takes student data and allocates a new struct element on the heap.
-student* createStudent(int id, const char* name, const char* surname, grade gradeOfStudent, const char* password);
+
+//The function takes student data and allocates a new struct element on the heap. Function is encrypting user password with Caesar cipher.
+student* createStudent(int id, const char* name, const char* surname, grade gradeOfStudent, const char* password,
+                       contactType conType,const char* correspondence);
 
 #endif
+
