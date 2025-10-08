1e828e8b97ff565d416a54e97886d6c2acc732ab
V0rt9X <vortexskyshaker@VorteXs-MacBook-Air.local>
2025-10-08 11:33:24 +0200

    feat: Add student database module.
    This commit introduces the core functionality for managing a student database using a linked list. It includes definitions for data structures and function prototypes for various operations.
    
    - Added a Node structure to represent elements in the linked list.
    - Declared functions for adding, printing, finding, and deleting students.
    - Declared functions for saving the database to a file and loading it from a file.
    - Added a function declaration for freeing the memory allocated to the list.

--- Deanery/modules/database.h
+++ Deanery/modules/database.h
@@ -1,4 +1,32 @@
 #ifndef FINALPROJECT_DATABASE_H
 #define FINALPROJECT_DATABASE_H
+#include <stdio.h>
+
+// Structure for storing student data in a chain of structures.
+typedef struct Node {
+    struct s *data;
+    struct Node *next;
+}Node;
+
+//Function adds an element to the end of a linked list.
+int student_add(Node* head);
+
+//Function prints the student data from the chain element.
+void student_print(Node* head);
+
+//Function finds a student by id in a linked list.
+Node* find_student_by_id(Node* head, unsigned id);
+
+//Function removes student by id from linked list.
+int delete_student_by_id(Node* head, unsigned id);
+
+//Function saves a linked list to a file.
+int save_to_file(Node* head);
+
+//Function unloads student data from a file into a linked list.
+int load_from_file(Node* head, FILE* fname);
+
+//Function frees memory from the linked list.
+void free_list(Node** head);
 
 #endif
